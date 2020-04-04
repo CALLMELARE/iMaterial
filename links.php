6c5c95fb3c89ff7b67931bafbc763ba1130ab05a
CALLMELARE <zuoyunfei@twt.edu.cn>
2020-04-04 12:33:48 +0800

    Update links.php

--- links.php
+++ links.php
@@ -1,14 +1,7 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 
 <div id="links">
-    <div class="links-title">
-        Friends
-    </div>
-    <div class="links-item">
-        <a href="http://www.kongjune.com/">KONGJUNE</a>
-        |
-        <a href="https://greyishsong.ink">greyishsong</a>
-        |
-        <a href="https://www.zrzz.site">X Mεl0n</a>
-    </div>
+    <!-- <div class="links-title">
+        <a href="www.callmelare.cn/blog/index.php/friends.html">Friends</a>
+    </div> -->
 </div>
